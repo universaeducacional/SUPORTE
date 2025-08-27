25bf6e2ec18254dd98657b1c8623b1ccc5a3f40d
universaeducacional <153693074+universaeducacional@users.noreply.github.com>
2025-08-27 11:33:19 -0300

    Delete configurar_noticias/import_webdriver.py

--- configurar_noticias/import_webdriver.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("--start-maximized")  # ou qualquer outro argumento que queira
-
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
